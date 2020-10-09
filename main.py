1af04123c286f86154f8cc9e2ff9891861d5b8bb
Navpreet Singh <nav.mandair7@gmail.com>
2020-10-09 11:13:52 +0000

    4th

--- main.py
+++ main.py
@@ -2,42 +2,47 @@ import pandas_datareader as pdr
 from datetime import datetime
 import calendar
 
-StartDates = [4, 5, 6, 7, 8]
-#Interval = "m"
+StartDates = [0, 1, 2, 3, 4]
+Interval = "d"
 #Ticker = "VGT"
 Amount = 50
 while 1:
   Ticker = input("Enter Ticker: ")
-  Interval = input("Enter Interval here on of (d,w,m): ")
-  #Amount = input("Enter Amount: ")
+  DefaultStartDate = datetime(1900, 1, 1).date()
+  StartDate = input("Enter Start Date (YYYY-mm-dd): ")
+  if StartDate == "":
+    StartDate = DefaultStartDate
+  else:
+    StartDate = datetime.strptime(StartDate, '%Y-%m-%d')
+  EndDate = datetime.now().date()
+  rawdata = pdr.get_data_yahoo(Ticker, start=StartDate, end=EndDate, interval=Interval)
+  rawdata['DATE'] = rawdata.index.to_pydatetime()
+  rawdata['WEEKDAY'] = rawdata['DATE'].dt.weekday
+  rawdata['Amount Invested'] = Amount
+  rawdata['Share Bought'] = Amount / rawdata['Close']
+  del rawdata['Open']
+  del rawdata['Low']
+  del rawdata['High']
+  del rawdata['Adj Close']
+  del rawdata['Volume']
 
-  for StartDay in StartDates:
-    StartDate = datetime(2010, 1, StartDay).date()
-    EndDate = datetime.now().date()
-    data = pdr.get_data_yahoo(Ticker, start=StartDate, end=EndDate, interval=Interval)
-
-    data['Amount Invested'] = Amount
-    data['Share Bought'] = Amount / data['Close']
-    del data['Open']
-    del data['Low']
-    del data['High']
-    del data['Adj Close']
-    del data['Volume']
-    
-    TotalAmountInvested = data['Amount Invested'].sum()
-    TotalShareBought = data['Share Bought'].sum()
-    AvgPrice = TotalAmountInvested / TotalShareBought
-    ActualStartDate = data.head(1).index.to_pydatetime()[0].date()
-    LastPrice = data.tail(1).get('Close').values[0]
-    #print(f"Ticker: {Ticker}  interval:  {Interval}" )
-    print(f"{ActualStartDate} {calendar.day_name[ActualStartDate.weekday()]} - {EndDate} {calendar.day_name[EndDate.weekday()]}")
-    #print(f"Total Amount Invested = {TotalAmountInvested}")
-    #print(f"Total Shares Own = {TotalShareBought}")
-    #print(f"Avg Price paid per share = {AvgPrice}")
-    #print(f"Total Value = {TotalShareBought*LastPrice}")
-    #print(f"Total Return $ = {(TotalShareBought*LastPrice)-TotalAmountInvested}")
-    TotalReturn = ((TotalShareBought * LastPrice) -
-                TotalAmountInvested) / TotalAmountInvested * 100
-    print("Total Return % = {:.2f}".format(TotalReturn))
+  for Day in StartDates:
+      data = rawdata.query(f'WEEKDAY == {Day}')
+      
+      TotalAmountInvested = data['Amount Invested'].sum()
+      TotalShareBought = data['Share Bought'].sum()
+      AvgPrice = TotalAmountInvested / TotalShareBought
+      ActualStartDate = data.head(1).index.to_pydatetime()[0].date()
+      LastPrice = data.tail(1).get('Close').values[0]
+      #print(f"StartDate: {StartDate} Ticker: {Ticker}  interval:  {Interval}" )
+      print(f"{ActualStartDate} {calendar.day_name[ActualStartDate.weekday()]} - {EndDate} {calendar.day_name[EndDate.weekday()]}")
+      #print(f"Total Amount Invested = {TotalAmountInvested}")
+      #print(f"Total Shares Own = {TotalShareBought}")
+      #print(f"Avg Price paid per share = {AvgPrice}")
+      #print(f"Total Value = {TotalShareBought*LastPrice}")
+      #print(f"Total Return $ = {(TotalShareBought*LastPrice)-TotalAmountInvested}")
+      TotalReturn = ((TotalShareBought * LastPrice) -
+                  TotalAmountInvested) / TotalAmountInvested * 100
+      print("Total Return % = {:.2f}".format(TotalReturn))
   print("-------------------------------------------------")
   print()
