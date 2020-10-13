77db0e3eed2660b5ddeff439695dbf073dd69fef
Navpreet Singh <nav.mandair7@gmail.com>
2020-10-13 00:46:49 +0000

    12th

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ def main():
     try:
       TickersInput = input("Enter Ticker: ")
       Tickers = list(TickersInput.split(",")) 
-      #info(Tickers)
       returns("yf", Tickers)
     except:
       print('Something went wrong ! try again')
     print()
 
-def info(Tickers):
-  for Ticker in Tickers:  
-    yf.Ticker(Ticker).history(period="max")
-    rawData = yf.Ticker(Ticker).recommendations.tail(20)
-    print(rawData)
+def info(Ticker):
+  infodata = yf.Ticker(Ticker).info
+  print(f"Symbol: {infodata['symbol']} Name: {infodata['shortName']}")
+  InstrumentType = infodata['quoteType']
+  if InstrumentType == 'ETF':
+    print(f"Category: {infodata['category']}")
+    print(f"Beta 3 Yr: {infodata['beta3Year']}")
+  else:
+    print(f"Sector: {infodata['sector']}")
+    print(f"Beta: {infodata['beta']}")
+
 
 def returns(API, Tickers):  
   StartDates = [0, 1, 2, 3, 4]
@@ -33,8 +38,9 @@ def returns(API, Tickers):
   CalBasedOn = input("Which data to use (Low, High, Open, Close, Adj Close) (default Close): ")
   if CalBasedOn == "":
     CalBasedOn = 'Close'
-    
+  
   for Ticker in Tickers:
+    info(Ticker)
     EndDate = datetime.now().date()
     if API == "yf":
       rawdata = yf.download(Ticker, start=StartDate, end=EndDate)
@@ -47,7 +53,6 @@ def returns(API, Tickers):
     rawdata['Share Bought'] = Amount / rawdata[CalBasedOn]
     del rawdata['Volume']
 
-    print(f"Ticker: {Ticker}" )
     for Day in StartDates:
       data = rawdata.query(f'WEEKDAY == {Day}')
       TotalAmountInvested = data['Amount Invested'].sum()
@@ -65,7 +70,7 @@ def returns(API, Tickers):
       TotalReturn = ((TotalShareBought * LastPrice) -
                       TotalAmountInvested) / TotalAmountInvested * 100
       TotalYearsInvested = ((ActualEndDate-ActualStartDate).days/365) 
-      print("Total Return = {:.2f}%, Total Avg Return is = {:.2f}% over {:.2f} years".format(TotalReturn, TotalReturn/TotalYearsInvested, TotalYearsInvested))
+      print("Total Return = {:.2f}%, Total Avg Return is = {:.2f}% p.a over {:.2f} years".format(TotalReturn, TotalReturn/TotalYearsInvested, TotalYearsInvested))
     print("-------------------------------------------------")
   
 
